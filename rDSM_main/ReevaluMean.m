bee1b584206d6674ae2a65db283f5fdcd1648707
Tianyu Wang <18804627006@163.com>
2023-07-20 21:42:37 +0800

    Update ReevaluMean.m

--- rDSM_main/ReevaluMean.m
+++ rDSM_main/ReevaluMean.m
@@ -4,6 +4,7 @@ function [PD, SimplexState] = ReevaluMean(PD,SimplexState,N,k)
 %fclose('all');
 %delete('D:\DSME\ReevaluationHistory.txt')
 
+
 %% Select the points and calculate the mean value    
     % --- The reevaluation point
     %[Point Coordinate value, Cost,ID];
@@ -13,19 +14,28 @@ function [PD, SimplexState] = ReevaluMean(PD,SimplexState,N,k)
     Dist = pdist([PD(end,1:N);Points]);
     IDree = find(Dist(1:size(Points,1))==0);
     %[Point Coordinate value, Cost,ID];
-    PreReevaluatedPoint =[PD(IDree(1),1:N),PD(IDree(1),N+2),PD(IDree(1),N+1)];
+    PreReevaluatedPoint =[PD(IDree,1:N),PD(IDree,N+2),PD(IDree,N+1)];
+    
     % --- Take the mean value of costs
     %[Point Coordinate value, Cost,ID];
-    NewPoint = [PD(end,1:N),mean([PD(end,N+2),PD(IDree(1),N+2)]),size(PD,1)+1];
-
+    NewPoint = [PD(end,1:N),mean([PD(end,N+2),PD(IDree,N+2)']),size(PD,1)+1];
 %% Output file
     fRH = fopen('Output\\ReevaluationHistory.txt','a+');
-    fprintf(fRH, '%s','Reevaluation points: p_1 | ... | p_N | cost| ID (before | after | mean)');
+    fprintf(fRH, '%s','Reevaluation points: p_1 | ... | p_N | cost| ID ');
     fprintf(fRH,'\r\n');
-    fprintf(fRH,'%.4f\t',PreReevaluatedPoint);
-    fprintf(fRH,'\r\n'); 
+    lines = size(PreReevaluatedPoint,1);
+    fprintf(fRH, '%s',' Pre-reevaluation points ');
+    fprintf(fRH,'\r\n');
+    for i  = 1:lines
+        fprintf(fRH,'%.4f\t',PreReevaluatedPoint(i,:));
+        fprintf(fRH,'\r\n'); 
+    end
+    fprintf(fRH, '%s',' Reevaluated points ');
+    fprintf(fRH,'\r\n');
     fprintf(fRH,'%.4f\t',ReevaluatedPoint);
-    fprintf(fRH,'\r\n'); 
+    fprintf(fRH,'\r\n');
+    fprintf(fRH, '%s',' New points ');
+    fprintf(fRH,'\r\n');
     fprintf(fRH,'%.4f\t',NewPoint);
     fprintf(fRH,'\r\n'); 
     fclose(fRH);
